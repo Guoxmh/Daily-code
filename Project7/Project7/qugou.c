eae454cf949d0882aff0014a8a2243d1d76bdc97
Guoxmh <1094185589@qq.com>
2019-07-20 22:08:32 +0800

    7/20

---
+++ Project7/Project7/qugou.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<Windows.h>
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+	while(1){
+		printf("Çü¹· \t Çü¹· \nŒÅÕ¨Ìì \n ²İÄâÂğ  Çü¹·  ÄãÂîÎÒ");
+		Sleep(1000);
+	}
+	system("pause");
+	return 0;
+}
